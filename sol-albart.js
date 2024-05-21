66b9a9087a19a3f6a686e7d50f1a203e1f915804
prjs <138359976+xukaluis1@users.noreply.github.com>
2024-05-21 13:22:58 +0700

    Add files via upload

--- sol-albart.js
+++ sol-albart.js
@@ -4,3 +4,45 @@ const menuBtn = document.querySelector('.menu-btn');
 menuBtn.addEventListener('click', () =>{
 	menu.classList.toggle('menu-open');
 });
+
+
+const chartData = {
+  labels: ["Presale", "Liquidity", "Burn", "Tax"],
+  data: [57, 27.6165, 15.3835, 0],
+};
+
+const myChart = document.querySelector(".my-chart");
+const ul = document.querySelector(".programming-stats .details ul");
+
+new Chart(myChart, {
+  type: "doughnut",
+  data: {
+    labels: chartData.labels,
+    datasets: [
+      {
+        label: " ",
+        data: chartData.data,
+      },
+    ],
+  },
+  options: {
+    borderWidth: 7,
+    borderRadius: 2,
+    hoverBorderWidth: 0,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+
+const populateUl = () => {
+  chartData.labels.forEach((l, i) => {
+    let li = document.createElement("li");
+    li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+    ul.appendChild(li);
+  });
+};
+
+populateUl();
